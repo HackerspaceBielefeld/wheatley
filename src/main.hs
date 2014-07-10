50b2e7c4b082876bda2297ed9127163445da29d2
gimbar <gimbar@breax.org>
2014-07-10 22:39:21 +0200

    copied pontarius echoclient example and done some modification

--- src/main.hs
+++ src/main.hs
@@ -8,15 +8,20 @@ import Control.Monad
 import Data.Default
 import System.Log.Logger
 
---updateGlobalLogger "Pontarius.Xmpp" $ setLevel DEBUG
-
-con = do
-            result <- session
-                     "jabber.space.bi"
-                     (Just (\_ -> ( [scramSha1 "wheatley" Nothing "wheatley"] )
-                           , Nothing))
-                     def
-            sess <- case result of
-                         Right s -> return s
-                         Left e  -> error $ "XmppFailure: " ++ show(e)
-            return sess
+main :: IO ()
+main = do
+   updateGlobalLogger "Pontarius.Xmpp" $ setLevel DEBUG
+   result <- session
+      "jabber.space.bi"
+      (Just (\_ -> ( [scramSha1 "wheatley" Nothing "wheatley"])
+         , Nothing))
+      def
+   sess <- case result of
+      Right s -> return s
+      Left e -> error $ "XmppFailure: " ++ (show e)
+   sendPresence def sess
+   forever $ do
+      msg <- getMessage sess
+      case answerMessage msg (messagePayload msg) of
+         Just answer -> sendMessage answer sess >> return ()
+         Nothing -> putStrLn "Received message with no sender."
