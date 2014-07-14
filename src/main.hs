3295b0a7071e88fc15909c988af62d52fa472e02
gimbar <gimbar@breax.org>
2014-07-15 01:31:43 +0200

    read config file

--- src/main.hs
+++ src/main.hs
@@ -4,7 +4,12 @@ module Main where
 
 import Network.Xmpp
 
+import Data.Aeson
+import Data.Text
+import qualified Data.ByteString.Lazy as B
+
 import Control.Monad
+import Control.Applicative
 import Data.Default
 import System.Log.Logger
 
@@ -13,15 +18,36 @@ main = do
    updateGlobalLogger "Pontarius.Xmpp" $ setLevel DEBUG
    result <- session
       "jabber.space.bi"
-      (Just (\_ -> ( [scramSha1 "wheatley" Nothing "wheatley"])
+      (Just (\_ -> [scramSha1 "wheatley" Nothing "wheatley"]
          , Nothing))
       def
    sess <- case result of
       Right s -> return s
-      Left e -> error $ "XmppFailure: " ++ (show e)
+      Left e -> error $ "XmppFailure: " ++ show e
    sendPresence def sess
    forever $ do
       msg <- getMessage sess
       case answerMessage msg (messagePayload msg) of
-         Just answer -> sendMessage answer sess >> return ()
+         Just answer -> void $ sendMessage answer sess 
          Nothing -> putStrLn "Received message with no sender."
+
+readXmppConfig :: String -> IO (Maybe XmppConfig)
+readXmppConfig "" = return Nothing
+readXmppConfig f  = do
+   s <- B.readFile f  
+   return ( decode s :: Maybe XmppConfig )
+
+data XmppConfig = XmppConfig {
+   server   :: Text,
+   username :: Text,
+   password :: Text,
+   channel  :: Text}
+   deriving Show
+
+instance FromJSON XmppConfig where
+   parseJSON (Object v) = XmppConfig      <$>
+                          v .: "server"   <*>
+                          v .: "username" <*>
+                          v .: "password" <*>
+                          v .: "channel"
+   parseJSON _          = mzero
