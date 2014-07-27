af3360a04d76d2196fc5d253fa7e48a169d0cfc6
gimbar <gimbar@breax.org>
2014-07-27 15:32:51 +0200

    first step of fixing dependencie collisions

--- src/TitleForLink.hs
+++ src/TitleForLink.hs
@@ -2,31 +2,34 @@
 
 module TitleForLink ( titleForLink ) where
 
-import Network.HTTP
-import Network.Browser
-import Network.URI
+import Network.HTTP.Conduit
 
+import Data.Maybe
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as C
 import Text.HTML.TagSoup
+import qualified Text.Regex as R
+
+isLink :: T.Text -> Bool
+isLink t = isJust $ R.matchRegex urlRegex $ T.unpack t 
+
+urlRegex :: R.Regex
+urlRegex = R.mkRegex "((https?):((//)|(\\\\\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)"
 
 titleForLink :: T.Text -> IO (Maybe T.Text)
 titleForLink "" = return Nothing
-titleForLink t  = case parseURI $ T.unpack t of
-                       Nothing -> return Nothing
-                       Just u  -> do
-                          (_, Response _ _ _ res) <- browse $ bAction u
-                          return $ extractTitle res 
+titleForLink t  = if isLink t then loadTitle t else return Nothing                              
+                              where loadTitle :: T.Text -> IO (Maybe T.Text)
+                                    loadTitle u = do
+                                       request <- simpleHttp $ T.unpack u
+                                       return $ extractTitle request
 
 extractTitle :: B.ByteString ->  Maybe T.Text
 extractTitle bs = do
    let tags = canonicalizeTags $ parseTags bs
-   case takeWhile (isTagCloseName "title") $ dropWhile (isTagOpenName "title") tags of
+   case takeWhile (not . isTagCloseName "title") $ dropWhile (not . isTagOpenName "title") tags of
         [] -> Nothing
         t  -> Just $ T.pack $ C.unpack $ foldl readTags "" t
           where readTags a (TagText b) = a `B.append` b
                 readTags a _           = a
-
-bAction :: URI -> BrowserAction (HandleStream B.ByteString) (URI, Response B.ByteString)
-bAction u = request $ mkRequest GET u
