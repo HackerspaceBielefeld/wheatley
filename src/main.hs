46ff53a7e86b3b4dca9ac1d0b6c8fac5065a5257
gimbar <gimbar@breax.org>
2014-09-17 22:42:03 +0200

    moved XmppConfig to it's own module

--- src/main.hs
+++ src/main.hs
@@ -5,17 +5,15 @@ module Main where
 import Network.Xmpp
 import Network.Xmpp.IM
 
-import Data.Aeson
 import qualified Data.Text as T
 import Data.Maybe
-import qualified Data.ByteString.Lazy as B
 
 import Control.Monad
-import Control.Applicative
 import System.Log.Logger
 import Control.Concurrent
 
 import TitleForLink
+import XmppConfig
 
 main :: IO ()
 main = do
@@ -74,7 +72,7 @@ displayTitleForLinks sess msg text = do
      where displayTitles :: [(Maybe T.Text,  T.Text)] -> IO ()
            displayTitles []                     = return ()
            displayTitles ((Just title, orig):x) = do
-              let body = MessageBody (messageLangTag msg) (title `T.append` (T.pack "\n") `T.append` orig)
+              let body = MessageBody (messageLangTag msg) (title `T.append` T.pack "\n" `T.append` orig)
               case answerMess msg body of
                    Nothing -> return () 
                    Just m  -> do
@@ -107,23 +105,4 @@ handlePresenceRequests sess = forever $ do
                      Left y  -> print y
                      Right _ -> return ()
 
-readXmppConfig :: String -> IO (Maybe XmppConfig)
-readXmppConfig "" = return Nothing
-readXmppConfig f  = do
-   s <- B.readFile f  
-   return ( decode s :: Maybe XmppConfig )
-
-data XmppConfig = XmppConfig {
-   server   :: T.Text,
-   username :: T.Text,
-   password :: T.Text,
-   channel  :: T.Text}
-   deriving Show
-
-instance FromJSON XmppConfig where
-   parseJSON (Object v) = XmppConfig      <$>
-                          v .: "server"   <*>
-                          v .: "username" <*>
-                          v .: "password" <*>
-                          v .: "channel"
-   parseJSON _          = mzero
+      
